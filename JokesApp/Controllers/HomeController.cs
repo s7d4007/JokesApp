f71229ed91cd44c8906c0ed0cc5bd2b3ac9ee5d5
s7d4007 <souvikdutta0775@outlook.com>
2025-12-08 22:08:07 +0530

    Added Joke of the Day to treat the user with a joke

--- JokesApp/Controllers/HomeController.cs
+++ JokesApp/Controllers/HomeController.cs
@@ -1,20 +1,36 @@
 using System.Diagnostics;
+using JokesApp.Data; // Ensure this matches your namespace
 using JokesApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace JokesApp.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ApplicationDbContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            // Logic to get a random joke from the database
+            var allJokes = await _context.Joke.ToListAsync();
+
+            if (allJokes.Any())
+            {
+                var random = new Random();
+                int index = random.Next(allJokes.Count);
+                var randomJoke = allJokes[index];
+
+                // Pass the joke to the View using ViewBag
+                ViewBag.JokeOfTheDay = randomJoke.JokeQuestion;
+                ViewBag.JokeOfTheDayAnswer = randomJoke.JokeAnswer;
+            }
+
             return View();
         }
 
